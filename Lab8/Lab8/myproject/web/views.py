bcfd795ad874dc9b8d1d729df2ea9857775f0eb2
xbrunner402-ux <xbrunner402@gmail.com>
2026-03-10 09:35:42 +0700

    Add files via upload

---
+++ Lab8/Lab8/myproject/web/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def form(request):
+    return render(request, 'form.html')
+
+def about(request):
+    return render(request, 'about.html')
